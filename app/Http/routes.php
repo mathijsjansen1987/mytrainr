e669b916ad51cfeb31c2e689b2f4f4375eba4ec8
mathijsjansen87 <mathijs@code.rehab>
2016-07-16 20:33:01 +0200

    router cleanup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,9 +10,19 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+
+///////////////////////////////////////////////////////////////////////////////
+//	IMPORTS
+///////////////////////////////////////////////////////////////////////////////
 use App\User;
 use App\Video;
 
+
+///////////////////////////////////////////////////////////////////////////////
+//	BACKEND ROUTES
+///////////////////////////////////////////////////////////////////////////////
+
+// URLS FOR GUESTS (NOT LOGGED IN)
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,32 +30,46 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
-Route::get('/videos', 'VideoController@index');
+
+// URLS ONLY FOR SPORTERS
+Route::group(['middleware' => 'role:sporter'], function () {
+	Route::get('/videos', 'VideoController@index');
+});
+
+// URLS ONLY FOR COACHES
+Route::group(['middleware' => 'role:coach'], function () {
+	// Route::get('/videos', 'VideoController@index');
+});
+
+// URLS ONLY FOR CLUBS
+Route::group(['middleware' => 'role:club'], function () {
+	// Route::get('/videos', 'VideoController@index');
+});
 
 
-// API
+///////////////////////////////////////////////////////////////////////////////
+//	API V1 ROUTES
+///////////////////////////////////////////////////////////////////////////////
 
 Route::group(['prefix' => 'api/v1/'], function () {
 
-// API V1 //////////////////////////
+	Route::get('users', ['middleware' => 'throttle:60,1', function (User $user) {
+		return $user::all();
+	}]);
 
-Route::get('users', ['middleware' => 'throttle:60,1', function (User $user) {
-    return $user::all();
-}]);
-
-// user video
-Route::get('user/{id}/videos', ['middleware' => 'throttle:60,1', function ($id) {
-    return User::find($id)->videos;
-}]);
+	// user video
+	Route::get('user/{id}/videos', ['middleware' => 'throttle:60,1', function ($id) {
+		return User::find($id)->videos;
+	}]);
 
 
-// user video analyses
-Route::get('user/videos/{id}/analyses', ['middleware' => 'throttle:60,1', function ($id) {
-    return Video::find($id)->analyses;
-}]);
+	// user video analyses
+	Route::get('user/videos/{id}/analyses', ['middleware' => 'throttle:60,1', function ($id) {
+		return Video::find($id)->analyses;
+	}]);
 
-Route::get('videos', ['middleware' => 'throttle:60,1', function (Video $video) {
-    return Video::all();
-}]);
+	Route::get('videos', ['middleware' => 'throttle:60,1', function (Video $video) {
+		return Video::all();
+	}]);
 
 });
