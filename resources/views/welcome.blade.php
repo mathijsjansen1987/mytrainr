9fba63886850269a81e8c283c3d25837723dddaf
mathijsjansen87 <mathijs@code.rehab>
2016-07-16 22:08:10 +0200

    template in dutch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Welkom bij mytrainr</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <p>Het open video annotatie platform om samen als clubs, trainers en sporters te leren over jouw sport.
+                    Op dit moment is mytrainr alleen gericht op de schaatssport, wil jij dat ook jouw sport wordt ondersteund neem dan contact op via: <a href="mailto:info@mytrainr.nl?subject=aanvraag andere sport">info@mytrainr.nl</a></p>
                 </div>
             </div>
         </div>
