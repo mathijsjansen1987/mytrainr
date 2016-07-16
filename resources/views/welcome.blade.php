db9e5b0c4b0a53ccc0f43d64ee49d9bf563cc512
mathijsjansen87 <mathijs@code.rehab>
2016-07-16 22:21:18 +0200

    cleanup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
 					<img src="assets/images/logo.png"/>
 					<p>Het open video annotatie platform om samen als clubs, trainers en sporters te leren over jouw sport.
 						Op dit moment is mytrainr alleen gericht op de schaatssport, wil jij dat ook jouw sport wordt ondersteund neem dan contact op via: <a href="mailto:info@mytrainr.nl?subject=aanvraag andere sport">info@mytrainr.nl</a></p>
-						<img src="assets/images/intro.jpg"/><br><br>
-						 <a href="{{ URL::to('/') }}/register" class="btn btn-default">Meteen registreren</a>
+					<img src="assets/images/intro.jpg"/><br><br>
+					<a href="{{ URL::to('/') }}/register" class="btn btn-default">Meteen registreren</a>
 				</div>
 			</div>
 		</div>
