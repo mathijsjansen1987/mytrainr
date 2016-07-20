2a7235734b7e83400916d1f769bbb0ef5a5db646
mathijsjansen87 <mathijs@code.rehab>
2016-07-20 21:03:15 +0200

    user seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,9 +11,10 @@ class UsersTableSeeder extends Seeder
      */
 	public function run()
 	{
+
 		DB::table('users')->insert([
-			'name' => 'Mathijs Jansen',
-			'email' => 'mathijs@code.rehab',
+			'name' => 'Ijsclub Lonneker',
+			'email' => 'ijsclub@lonneker.nl',
 			'password' => bcrypt('pass'),
 		]);
 
@@ -22,5 +23,49 @@ class UsersTableSeeder extends Seeder
 			'email' => 'getenkampe@hotmail.com',
 			'password' => bcrypt('pass'),
 		]);
+
+		DB::table('users')->insert([
+			'name' => 'Mathijs Jansen',
+			'email' => 'mathijs@code.rehab',
+			'password' => bcrypt('pass'),
+		]);
+
+
+		DB::table('roles')->insert([
+			'id' => 1,
+			'name' => 'club',
+			'display_name' => 'Club',
+			'description' => 'Club'
+		]);
+
+		DB::table('roles')->insert([
+			'id' => 2,
+			'name' => 'coach',
+			'display_name' => 'Coach',
+			'description' => 'coach'
+		]);
+
+		DB::table('roles')->insert([
+			'id' => 3,
+			'name' => 'sporter',
+			'display_name' => 'Sporter',
+			'description' => 'Sporter'
+		]);
+
+		DB::table('role_user')->insert([
+			'user_id' => 1,
+			'role_id' => 1,
+		]);
+
+		DB::table('role_user')->insert([
+			'user_id' => 2,
+			'role_id' => 2,
+		]);
+
+		DB::table('role_user')->insert([
+			'user_id' => 3,
+			'role_id' => 3,
+		]);
+
 	}
 }
